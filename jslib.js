7b52b2d47d00bfd5ff6710fc548735c5d769959b
mightyplow <mightyplow@gmail.com>
2016-07-24 12:50:36 +0200

    moved library files into folder

---
+++ jslib.js
@@ -0,0 +1,7 @@
+module.exports = {
+    file: require('./lib/file'),
+    html: require('./lib/html'),
+    object: require('./lib/object'),
+    string: require('./lib/string'),
+    stylesheet: require('./lib/stylesheet')
+}
