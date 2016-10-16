b627bd45979190cd35bb3852c1669be63c31060a
mightyplow <mightyplow@gmail.com>
2016-10-16 02:21:30 +0200

    added function lib with combine function

--- jslib.js
+++ jslib.js
@@ -3,6 +3,7 @@
 module.exports = {
     array: require('./lib/array'),
     file: require('./lib/file'),
+    function: require('./lib/function'),
     html: require('./lib/html'),
     object: require('./lib/object'),
     string: require('./lib/string'),
